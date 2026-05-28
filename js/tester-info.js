bd8b4d82efb6e90e25e7e79bf28c7030e84bf2c1
walkaplex <walkmail@hotmail.com>
2026-05-28 14:51:42 -0700

    Add in-app tester checklist

--- js/tester-info.js
+++ js/tester-info.js
@@ -1,21 +1,26 @@
 (() => {
-  const openButton = document.getElementById("testerInfoBtn");
-  const modal = document.getElementById("testerInfoModal");
-  const closeButton = document.getElementById("testerInfoCloseBtn");
+  function bindModal(openId, modalId, closeId) {
+    const openButton = document.getElementById(openId);
+    const modal = document.getElementById(modalId);
+    const closeButton = document.getElementById(closeId);
 
-  function openInfo() {
-    if (modal) modal.classList.add("show");
+    function open() {
+      if (modal) modal.classList.add("show");
+    }
+
+    function close() {
+      if (modal) modal.classList.remove("show");
+    }
+
+    if (openButton) openButton.addEventListener("click", open);
+    if (closeButton) closeButton.addEventListener("click", close);
+    if (modal) {
+      modal.addEventListener("click", event => {
+        if (event.target === modal) close();
+      });
+    }
   }
 
-  function closeInfo() {
-    if (modal) modal.classList.remove("show");
-  }
-
-  if (openButton) openButton.addEventListener("click", openInfo);
-  if (closeButton) closeButton.addEventListener("click", closeInfo);
-  if (modal) {
-    modal.addEventListener("click", event => {
-      if (event.target === modal) closeInfo();
-    });
-  }
+  bindModal("testerInfoBtn", "testerInfoModal", "testerInfoCloseBtn");
+  bindModal("testPlanBtn", "testPlanModal", "testPlanCloseBtn");
 })();
