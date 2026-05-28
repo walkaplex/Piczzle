52648f49bf145363d6e37a912745ff541fbb5cbe
walkaplex <walkmail@hotmail.com>
2026-05-28 16:08:18 -0700

    Keep completed puzzle viewable

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const APP_SHELL = [
   "/Piczzle/js/native.js?v=20260520",
   "/Piczzle/js/share-config.js?v=20260527",
   "/Piczzle/js/share-cloud.js?v=20260527-cloud2",
-  "/Piczzle/js/app.js?v=20260528-safety1",
+  "/Piczzle/js/app.js?v=20260528-complete1",
   "/Piczzle/js/share-feedback.js?v=20260528-feedback1",
   "/Piczzle/js/pwa.js?v=20260527-cache",
   "/Piczzle/assets/app-icon.svg",
